4ce991c5f9f75ca2cb35971f659107f38c3d7f5f
potatoonions <darkenbrine@gmail.com>
2026-03-04 17:14:03 +0800

    yesness

--- debate_trainer/debate_trainer/urls.py
+++ debate_trainer/debate_trainer/urls.py
@@ -4,9 +4,11 @@ from django.urls import include, path
 
 from trainer.views import home_view
 from trainer.debate_chat import debate_chat_view
+from trainer.formal_debate_ui_enhanced import formal_debate_view
 
 urlpatterns = [
-    path("", debate_chat_view, name="home"),  # Direct to debate trainer
+    path("", formal_debate_view, name="home"),  # Enhanced debate trainer
+    path("chat/", debate_chat_view, name="chat"),  # Old chat view
     path("api-docs/", home_view, name="api-docs"),  # API documentation
     path("admin/", admin.site.urls),
     path("api/", include("trainer.urls")),
